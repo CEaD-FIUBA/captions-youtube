84c98a0cea7353a04690803ccffb4958c05d5931
cristian3629 <cristian3629@gmail.com>
2017-10-02 17:07:21 +0000

    ya funciona el parseo

--- front-end/captionParse.js
+++ front-end/captionParse.js
@@ -8,27 +8,28 @@ function parserCaption(caption){
 
   var array = caption_string.split(",");
   var len = array.length;
-   for (var i = 0; i < len; i++) {
-     var time = array[i];
-     var text = array[i+1];
-     var line = $("<p></p>");
-     line.val(time);
-    //Aca me fijo si mi caption es un indice o un caption
-    var n = text.search("Tema:");
-    //Si no es un indice
-    if (n == -1){
-      line.text(text);
-      line.addClass('line');
-      $("#caption").append(line);
-      captions.push(line);
-    }else{
-      var chapter_name = text.split(":")[1];
-      line.text(chapter_name);
-      //console.log("chapter_name:"+chapter_name);
-      line.addClass('chapter');
-      $("#chapters").append(line);
-      timesOfChapters.push(time);
-      chapters.push(line);
+   for (var i = 0; i < len-1; i=i+2) {
+        var time = array[i];
+        var text = array[i+1];
+        var line = $("<p></p>");
+        line.val(time);
+        //Aca me fijo si mi caption es un indice o un caption
+        var n = text.search("Tema:");
+        //Si no es un indice
+        if (n == -1){
+          line.text(text);
+          line.addClass('line');
+          $("#caption").append(line);
+          captions.push(line);
+        }else{
+          var chapter_name = text.split(":")[1];
+          line.text(chapter_name);
+          //console.log("chapter_name:"+chapter_name);
+          line.addClass('chapter');
+          $("#chapters").append(line);
+          timesOfChapters.push(time);
+          chapters.push(line);
     }
+    console.log('text: '+text+'time: '+time);
   }
 }
